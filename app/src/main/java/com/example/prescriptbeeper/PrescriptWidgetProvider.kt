793bf93c379cbc2ac59b01cfdbdfc3d23f4a0a0f
GalaxyJam <66970814+GalaxyJamSpeed@users.noreply.github.com>
2026-09-11 12:48:10 +0300

    [Bug Fixes] - ...accidentally broke the Widget while adding the icon, fixed now.

--- app/src/main/java/com/example/prescriptbeeper/PrescriptWidgetProvider.kt
+++ app/src/main/java/com/example/prescriptbeeper/PrescriptWidgetProvider.kt
@@ -44,6 +44,7 @@ class PrescriptWidgetProvider : AppWidgetProvider() {
             }
 
             val views = RemoteViews(context.packageName, R.layout.widget_prescript)
+            views.setTextViewText(R.id.widgetCountText, "$count")
             views.setImageViewResource(R.id.widgetCompletedIcon, R.drawable.iccompleted)
             views.setTextViewText(R.id.widgetLabelText, "STAGE $stage")
             views.setImageViewResource(R.id.widgetStageIcon, icon)
